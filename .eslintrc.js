472ac9c2fcd8b9a1394bbe48ac8d4e037c22e112
xinda.zeng <1692488900@qq.com>
2022-05-06 00:08:29 +0800

    :heavy_plus_sign: 使用并配置eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    "no-underscore-dangle": 'off',
+  }
+}
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, defineProps } from 'vue'
 
 defineProps<{ msg: string }>()
 
