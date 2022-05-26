c360d40064b686b789574994b4ed152f7f881aee
Sinda <1692488900@qq.com>
2022-05-26 14:35:14 +0800

    :art: 规范代码,适配vscode vite插件

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  /** 端口 */
+  readonly VITE_PORT: number
+  /** 路由模式 */
+  readonly VITE_ROUTER_HISTORY: string
+  /** 接口地址 */
+  readonly VITE_BASE_API: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
