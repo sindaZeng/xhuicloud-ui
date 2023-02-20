5512a4a135fac124ca8284278ee752e33e55b7fc
sindaZeng <sindazeng@gmail.com>
2023-02-20 12:04:21 +0800

    :bricks: 预构建

--- vite.config.ts
+++ vite.config.ts
@@ -47,16 +47,8 @@ export default defineConfig({
     }
   },
   define: {
+    global: {},
     APP_INFO: JSON.stringify(APP_INFO)
-  },
-  optimizeDeps: {
-    include: [
-      '@vue/runtime-core',
-      '@vue/shared',
-      'lodash',
-      'element-plus/es/locale/lang/zh-cn',
-      'element-plus/es/locale/lang/en'
-    ]
   },
   plugins: [
     vue(),
@@ -91,5 +83,79 @@ export default defineConfig({
     //   filename: 'stats.html', //分析图生成的文件名
     //   open: false //如果存在本地服务端口，将在打包后自动展示
     // })
-  ]
+  ],
+  optimizeDeps: {
+    include: [
+      'vue',
+      'pinia',
+      'sass',
+      'lodash',
+      'vue-router',
+      '@vue/runtime-core',
+      '@vue/shared',
+      'crypto-js',
+      'element-plus/es',
+      'element-plus/es/locale/lang/zh-cn',
+      'element-plus/es/locale/lang/en',
+      'element-plus/es/components/row/style/css',
+      'element-plus/es/components/switch/style/css',
+      'element-plus/es/components/tooltip/style/css',
+      'element-plus/es/components/color-picker/style/css',
+      'element-plus/es/components/form-item/style/css',
+      'element-plus/es/components/notification/style/css',
+      'element-plus/es/components/icon/style/css',
+      'element-plus/es/components/breadcrumb/style/css',
+      'element-plus/es/components/infinite-scroll/style/css',
+      'element-plus/es/components/breadcrumb-item/style/css',
+      'element-plus/es/components/divider/style/css',
+      'element-plus/es/components/form/style/css',
+      'element-plus/es/components/col/style/css',
+      'element-plus/es/components/card/style/css',
+      'element-plus/es/components/button/style/css',
+      'element-plus/es/components/config-provider/style/css',
+      'element-plus/es/components/container/style/css',
+      'element-plus/es/components/main/style/css',
+      'element-plus/es/components/header/style/css',
+      'element-plus/es/components/date-picker/style/css',
+      'element-plus/es/components/drawer/style/css',
+      'element-plus/es/components/image/style/css',
+      'element-plus/es/components/image/style/css',
+      'element-plus/es/components/table/style/css',
+      'element-plus/es/components/table-column/style/css',
+      'element-plus/es/components/input/style/css',
+      'element-plus/es/components/dropdown/style/css',
+      'element-plus/es/components/popover/style/css',
+      'element-plus/es/components/dropdown-item/style/css',
+      'element-plus/es/components/dropdown-menu/style/css',
+      'element-plus/es/components/pagination/style/css',
+      'element-plus/es/components/scrollbar/style/css',
+      'element-plus/es/components/dialog/style/css',
+      'element-plus/es/components/loading/style/css',
+      'element-plus/es/components/tabs/style/css',
+      'element-plus/es/components/tab-pane/style/css',
+      'element-plus/es/components/select/style/css',
+      'element-plus/es/components/option/style/css',
+      'element-plus/es/components/checkbox-group/style/css',
+      'element-plus/es/components/radio-group/style/css',
+      'element-plus/es/components/checkbox/style/css',
+      'element-plus/es/components/radio-button/style/css',
+      'element-plus/es/components/upload/style/css',
+      'element-plus/es/components/collapse/style/css',
+      'element-plus/es/components/collapse-item/style/css',
+      'element-plus/es/components/backtop/style/css',
+      'element-plus/es/components/avatar/style/css',
+      'element-plus/es/components/menu/style/css',
+      'element-plus/es/components/menu-item/style/css',
+      'element-plus/es/components/sub-menu/style/css',
+      'element-plus/es/components/tag/style/css',
+      'element-plus/es/components/empty/style/css',
+      'element-plus/es/components/descriptions/style/css',
+      'element-plus/es/components/descriptions-item/style/css',
+      'element-plus/es/components/radio/style/css',
+      'element-plus/es/components/space/style/css',
+      'element-plus/es/components/alert/style/css',
+      'element-plus/es/components/cascader/style/css',
+      'element-plus/es/components/tree/style/css'
+    ]
+  }
 })
